076886f39493b8ee49eda0e4ff21550214220ad2
Saagar Shah <saagarshahs@hotmail.com>
2021-08-17 21:57:55 +0100

    Converted inputText variable from global to local

--- calculate.js
+++ calculate.js
@@ -1,16 +1,16 @@
 // TODO: convert to arrow functions
 
 //global variables
-let calculateInputText = document.getElementById("txtCalculator");
-let firstNumber = [0, false];
-let secondNumber = 0;
-let runningTotal = 0;
-let pendingOperation;
+let firstNumber = []; let secondNumber;
+let pendingOperation; let runningTotal;
+
+
 
 //receiving a number as input and adding to textbox
 function receiveNumber(numberInput)
 {
   event.preventDefault();
+  let calculateInputText = document.getElementById("txtCalculator");
   
   if(!firstNumber[1])
   {
@@ -29,6 +29,8 @@ function receiveNumber(numberInput)
 //numbers generally don't start with 0
 function removeStartingZero()
 {
+  let calculateInputText = document.getElementById("txtCalculator");
+  
   if(calculateInputText.value[0] == 0) 
   {
     calculateInputText.value -= calculateInputText.value[0];
@@ -42,26 +44,9 @@ function receiveOperation(operationInput)
   
   firstNumber[1] = true; //so the program knows to move on to the next number
   
-  pendingOperation = operationInput;
+  pendingOperation = operationInput;  
   
-  calculateInputText.value = "0";
-}
-
-//performing the requested calculation by running through a series of switch cases
-function whichOperation()
-{
-  firstNumber[0] = parseFloat(firstNumber[0]);
-  secondNumber = parseFloat(calculateInputText.value);
-  
-  switch(pendingOperation)
-  {
-    case("+"): runningTotal = firstNumber[0] + secondNumber; break;
-    case("-"): runningTotal = firstNumber[0] - secondNumber; break;
-    case("*"): runningTotal = firstNumber[0] * secondNumber; break;
-    case("/"): runningTotal = firstNumber[0] / secondNumber; break;
-    case("%"): runningTotal = (firstNumber[0]/100) * secondNumber; break;
-    case("root"): runningTotal = Math.sqrt(secondNumber); break;
-  }
+  document.getElementById("txtCalculator").value = "0";
 }
 
 //running the final calculation and loading answer into textbox
@@ -69,19 +54,38 @@ function calculateSum()
 {
   event.preventDefault();
 
-  whichOperation();
+  whichOperation();  
   
-  calculateInputText.value = runningTotal;
+  document.getElementById("txtCalculator").value = runningTotal;
   removeStartingZero();
 }
 
 //resetting current calculator to 0 (default state upon loading page)
 function resetCalculator()
 {
-  calculateInputText.value = 0;
+  document.getElementById("txtCalculator").value = 0;
   runningTotal = 0;
   
   firstNumber[0] = 0; firstNumber[1] = false;
   secondNumber = 0;
 }
 resetCalculator();
+
+
+
+//performing the requested calculation by running through a series of switch cases
+function whichOperation()
+{
+  firstNumber[0] = parseFloat(firstNumber[0]);
+  secondNumber = parseFloat(document.getElementById("txtCalculator").value);
+  
+  switch(pendingOperation)
+  {
+    case("+"): runningTotal = firstNumber[0] + secondNumber; break;
+    case("-"): runningTotal = firstNumber[0] - secondNumber; break;
+    case("*"): runningTotal = firstNumber[0] * secondNumber; break;
+    case("/"): runningTotal = firstNumber[0] / secondNumber; break;    
+    case("%"): runningTotal = (firstNumber[0]/100) * secondNumber; break;
+    case("root"): runningTotal = Math.sqrt(secondNumber); break;
+  }
+}
